9b71bcd570ae7ebe8ecd6b5d7c23dc7f941d4222
Kazuki Yokoyama <kmyokoyama@inf.ufrgs.br>
2020-04-25 23:57:49 -0300

    Add sketch of load the rules from shapass API

--- main.go
+++ main.go
@@ -1,14 +1,83 @@
 package main
 
 import (
+	"bytes"
 	"crypto/sha256"
 	"encoding/base64"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"net/http"
+	"os"
+
 	"github.com/atotto/clipboard"
 	"golang.org/x/crypto/ssh/terminal"
-	"os"
 )
+
+const apiUrl = "https://shapass.com/api"
+
+type Rule struct {
+	Name      string
+	Length    int
+	Prefix    string
+	Suffix    string
+	Algorithm string
+	Metadata  map[string]string
+}
+
+type ListResponse struct {
+	Status string
+	Rules  []Rule
+	Code   int
+}
+
+type LoginRequest struct {
+	Email    string
+	Password string
+}
+
+type LoginResponse struct {
+	Status string
+	Token  string
+}
+
+type TokenRequest struct {
+	Token string
+}
+
+func login(email string, password string) (string, error) {
+	loginRequest := LoginRequest{Email: email, Password: password}
+	reqBody := new(bytes.Buffer)
+	json.NewEncoder(reqBody).Encode(loginRequest)
+
+	resp, err := http.Post(apiUrl+"/login", "application/json", reqBody)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	var loginResponse LoginResponse
+	json.NewDecoder(resp.Body).Decode(&loginResponse)
+
+	return loginResponse.Token, nil
+}
+
+func list(token string) ([]Rule, error) {
+	listRequest := TokenRequest{Token: token}
+	reqBody := new(bytes.Buffer)
+	json.NewEncoder(reqBody).Encode(listRequest)
+
+	resp, err := http.Post(apiUrl+"/list", "application/json", reqBody)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var listResponse ListResponse
+	json.NewDecoder(resp.Body).Decode(&listResponse)
+
+	return listResponse.Rules, nil
+}
 
 func makeSecret(parts ...[]byte) (secret []byte) {
 	for _, part := range parts {
@@ -24,13 +93,46 @@ func encode(secret []byte) string {
 	return base64.StdEncoding.EncodeToString(ss[:])
 }
 
+func fetchServiceFromAPI(email string, masterPasswd string) (*Rule, error) {
+	token, err := login("your@email.com", "your-shapass-generated-password")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	rules, err := list(token)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	for _, rule := range rules {
+		if rule.Name == "service" {
+			return &rule, nil
+		}
+	}
+
+	return nil, fmt.Errorf("shapass: Service not found")
+}
+
 func main() {
 	lengthOpt := flag.Int("len", 10, "Length of the password")
 	suffixOpt := flag.String("suffix", "", "Suffix to generate the output password (default \"\")")
 	showOpt := flag.Bool("show", false, "Should show output password? (default false)")
-	clipOpt := flag.Bool("clip", true, "Should copy output password to system clipboard?")
+	clipOpt := flag.Bool("clip", true, "Should copy output password to system clipboard? (default true)")
+	setupOpt := flag.Bool("setup", false, "Should fetch and setup? (default false)")
 
 	flag.Parse()
+
+	if *setupOpt {
+		rule, err := fetchServiceFromAPI("yokoyama.km@gmail.com", "H8TAc/fO3M0MgD9D0vfk55AK4YQkeRUZ")
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		fmt.Println(rule)
+	}
 
 	if flag.NArg() != 1 {
 		fmt.Println("Error: Provide exactly 1 service as last argument")
